d0697c6370464eb8227fc326d9a7c8caa0057e64
JFRiddle3618 <JFRiddle3618@gmail.com>
2018-05-31 04:21:39 +0000

    user story #6001: task #5003: Zoe made a change to the test at 11:21pm

--- src/main/java/LeanFtTest.java
+++ src/main/java/LeanFtTest.java
@@ -224,7 +224,7 @@ public class LeanFtTest extends UnitTestClassBase {
           //      .innerText("dvantage ")
             //    .tagName("SPAN").build());
         WebElement dvantageWebElement = browser.describe(WebElement.class, new WebElementDescription.Builder()
-                .innerText("dvantage ")
+                .innerText("dvantag ")
                 .tagName("SPAN").build());
 
 //hello
